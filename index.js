c39c299382d8d24e9361f7fd49c9328f8edff13d
Travis Mendez <Travis.mendez779@gmail.com>
2022-08-17 15:14:42 -0700

    commit tree ba43370b2547476326b4e37782814cb2dbdc6af1

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random()*10);
+console.log(random);
+
+let num3 = 64 
+let num4 = 16 
+let mod = num3 / num4;
+
+let max = Math.max(1, 2, 3, 4, 20);
